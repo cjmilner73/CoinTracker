1c22eb841efc3735c297fb0fcce8ce7a20033895
cjmilner73 <chris.milner@gmail.com>
2017-04-27 04:24:27 +0800

    Added loadBalance.py and rawData.py

---
+++ loadBalance.py
@@ -0,0 +1,39 @@
+import json
+import datetime
+import time
+import calendar
+import adx
+from poloniex import Poloniex
+from pymongo import MongoClient
+from bson import ObjectId
+
+polCon =  Poloniex("AQKCQME8-FA6XJ0XX-Z81BZC8N-ARJV85F9","b607e01152b255a4d1da89e01c5fd7cd6c8d5784db5c4c369875c1d48cea5b9cd1c26cffa5122bc43b293df12e04f2adeae10847190abaaf21963331838893e8")
+
+client = MongoClient('mongodb://localhost:27017')
+
+currTime = time.strftime("%Y/%m/%d %H:%M:%S")
+
+db = client.ticker_db
+
+polBal = polCon.returnBalances()
+
+
+polTickers = polCon.returnTicker()
+tickPairKeys = polTickers.keys()
+
+total = 0
+
+myBalance = {}
+
+for balTicker, balValue  in polBal.iteritems():
+   for tickerPair, price in polTickers.iteritems():
+      btcKey = 'BTC_' + balTicker
+      if (tickerPair == btcKey):
+          floatBalValue = float(balValue)
+          if ( floatBalValue != 0):
+               myBalance[btcKey] = floatBalValue
+               total += float(price['last']) * floatBalValue
+
+myBalance['timestamp'] = currTime
+myBalance['totalBTC'] = total
+db.balances.insert(myBalance)
