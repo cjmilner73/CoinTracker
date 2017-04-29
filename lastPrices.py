b30da68a4172d17e91dec9376e1772595938000d
cjmilner73 <chris.milner@gmail.com>
2017-04-30 04:45:24 +0800

    Added polCon referennces

--- lastPrices.py
+++ lastPrices.py
@@ -2,13 +2,11 @@ import ast
 import json
 import datetime
 import time
-import adx
 from poloniex import Poloniex
 from pymongo import MongoClient
-from bson import ObjectId
+from myConnection import getConn
 
-
-polCon =  Poloniex("AQKCQME8-FA6XJ0XX-Z81BZC8N-ARJV85F9","b607e01152b255a4d1da89e01c5fd7cd6c8d5784db5c4c369875c1d48cea5b9cd1c26cffa5122bc43b293df12e04f2adeae10847190abaaf21963331838893e8")
+polCon =  getConn()
 
 client = MongoClient('mongodb://localhost:27017')
 
---
+++ myConnection.py
@@ -0,0 +1,5 @@
+from poloniex import Poloniex
+
+def getConn():
+    polCon =  Poloniex("AQKCQME8-FA6XJ0XX-Z81BZC8N-ARJV85F9","b607e01152b255a4d1da89e01c5fd7cd6c8d5784db5c4c369875c1d48cea5b9cd1c26cffa5122bc43b293df12e04f2adeae10847190abaaf21963331838893e8")
+    return polCon
